5c7fe080a4868c1f18a6f5ece8ceeeae45272a02
Tom Armstrong <thomasarmstrong98@gmail.com>
2023-04-12 21:21:59 +0100

    change default num processes

--- scripts/drafting/generate_mcts_examples.py
+++ scripts/drafting/generate_mcts_examples.py
@@ -60,8 +60,8 @@ def save_examples_locally(
 def main(
     number_of_self_play_rounds: int = 50,
     tree_reset_interval: int = 10,
-    number_of_parrallel_trees: int = 8,
-    number_of_processes: int = 8,
+    number_of_parrallel_trees: int = 4,
+    number_of_processes: int = 4,
     time_limit_per_tree_seconds: int = 30,
 ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     examples = list()
